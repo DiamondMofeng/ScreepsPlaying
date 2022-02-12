56421de023071974e8289d5bf10abbc4b347e3e5
Mofeng <363034937@qq.com>
2022-02-12 21:54:01 +0800

    working on remote energy base script

--- default/script_outerEnergyBase.js
+++ default/script_outerEnergyBase.js
@@ -1,8 +1,38 @@
 /*
 param:baseRoom,targetRoom
-思路:用pos.findPathTo从storage寻找通向其他room内资源点的路径:
+思路:
+TODO 暂时手动定义起终点
+首先用`Game.map.findRoute`寻找路径房间，把结果存到memory.outerBase中
+再用PathFinder.search初步寻找路径，把每个房间的第一个/最后一个点记录，用于后续优化路径
+对于结果房间，从起点房间开始依次处理。
+  首先，检查正在处理的房间是否有视野(在rooms中)
+  若无视野，则：
+    observer开视野；派creep去开视野
+  有视野后，
+    用`PathFinder.search`找路，方便利用现有道路、规避建筑等，
+    ? 同时一定程度上降低沼泽的权重方便修近路 ?
+    * 1.controller
+    找路前，挑选一个点 距离controller为1内的 距离入口最近的点作为后续cliamer的工作位置
+    第一遍找路通向controller，调整cliamer的工作位置cost为256，放置一条通向controller的 道路 的 建筑工地，
+    
+    * 2.现在开始处理sources
+    对于source,获取后存入数组，依次处理。
+    寻路前，放置一个container,位置为 (?实现) 距离source为1内的离入口最近的点
+    进行 第 2 - ? 遍寻路 找路通向source。此时调整 Road建筑工地 的cost等同道路，container建筑工地 的cost为最大。找到后放置道路 的 建筑工地
 
-若没有observer,需要
+    * 3.记录路径
+    最后记录路径供后续工作bot行动。
+    claimer: path_toController
+    harvester: path_toSource 1、2
+    carrier: path_toSource 、 (? path_fromSource(toStorage))
+    ? 大概没必要单独存一个反向的
+
+! 派遣pionner去修建筑
+    
+  
+    // 起点：
+    // 终点：
+
 room.storage.pos.findPathTo
 
 
@@ -107,7 +137,7 @@ function pathFinder(fromPos, toPos) {
 
 
 
-function buildEnergyBase(roomToBuild='') {
+function buildEnergyBase(roomToBuild = '') {
 
   let resule = pathFinder(Game.flags['findStarter'].pos, Game.flags['findEnder'].pos)
 
