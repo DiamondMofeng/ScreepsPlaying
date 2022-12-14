cc84e7fb0e1b5cb3c2987c01e7afcd62d1d4d0bb
Mofeng <363034937@qq.com>
2022-12-14 20:36:55 +0800

    delete unnecessary files

--- src/test/copilot.js
+++
@@ -1,1 +0,0 @@
-//superbitch bot
--- src/test/test.js
+++
@@ -1,5 +0,0 @@
-Game.creeps['carrier35705013'].moveTo(Game.flags['Flag12']);
-
-Game.creeps['carrier35705013'].pickup(Game.getObjectById("62025b2e550c140ce03038e0"))
-
-RESOURCE_GHODIUM_OXIDE
