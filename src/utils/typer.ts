4aa9959ad310e48beafba1b29ff7250ec4d2e549
Mofeng <363034937@qq.com>
2022-12-26 02:37:28 +0800

    feat: new util typer: `looseConcat`

--- src/utils/typer.ts
+++ src/utils/typer.ts
@@ -7,6 +7,14 @@ export function includes<T extends U, U>(arr: ReadonlyArray<T>, item: U): item i
 }
 
 /**
+ * Looser version of Array.concat().  
+ * Concatenating arrays of different types.
+ */
+export function looseConcat<T, U>(arr1: T[], ...items: (T | U | ConcatArray<T | U>)[]): (T | U)[] {
+  return (arr1 as (T | U)[]).concat(...items);
+}
+
+/**
  * Can be used as filter callback for convenience.
  * Solves equations of the form a * x = b
  * @example
@@ -34,3 +42,4 @@ export type resInStore<T> = T extends Store<infer R, any> ? R[] : never[];
 export function resourceTypesIn<T extends Store<ResourceConstant, any>>(store: T): resInStore<T> {
   return Object.keys(store) as resInStore<T>;
 }
+
